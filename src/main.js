2151b38bca794295d1f047398b0bdbc88be131fe
PickiUp <lzy2001@proton.me>
2023-03-09 20:27:17 +0800

    修复 因elementplus版本过高报错，引入低版本

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/main.css'
+// 全局导入 element ui图标
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 // 导入路由
 import router from "./router/index";
@@ -9,4 +10,4 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
 }
 // 挂载路由到 vue 实例
-createApp(App).use(router).mount('#app')
+app.use(router).mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-
+// 自动导入 element ui图标
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
