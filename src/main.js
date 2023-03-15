913da132ed4e555a0b5e78c34c33809265d4a944
PickiUp <lzy2001@proton.me>
2023-03-15 21:27:40 +0800

    全局导入 elementplus组件（不包括icon）

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './assets/main.css'
+// import './assets/main.css'
 // 全局导入 element ui图标
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+// 全局导入 ui库
+import ElementPlus from 'element-plus'
+
 // 导入路由
 import router from "./router/index";
 const app = createApp(App)
@@ -10,4 +13,6 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
 }
 // 挂载路由到 vue 实例
-app.use(router).mount('#app')
+app.use(router)
+app.use(ElementPlus)
+app.mount('#app')
