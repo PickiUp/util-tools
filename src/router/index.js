fa88f8c2e9663dc40bdfa47cbe32bc0ea633a2fa
pickiup <lzy2001@proton.me>
2023-05-04 19:57:30 +0800

    fix: 修改路径

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ const router = createRouter({
     history: createWebHistory('/util-tools/'),
     routes: [
         { name: "home", path: "/", component: () => import("../views/HomePage") },
-        { name: "MultipleSearch", path: "/MultipleSearch", component: () => import("../views/MultipleSearch") },
+        { name: "MultipleSearch", path: "/muti-search", component: () => import("../views/MultipleSearch") },
         { name: "ObsidianVariousComplement", path: "/ob-dict-generator", component: () => import("../views/ObsidianVariousComplement") },
     ]
 })// 导出 路由 
