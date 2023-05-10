12ad472de13d34d8cdb4ebafaee707b8c2e16ff4
pickiup <lzy2001@proton.me>
2023-05-10 10:15:02 +0800

    modify: 修改路径为驼峰命名

--- src/router/index.js
+++ src/router/index.js
@@ -5,8 +5,8 @@ const router = createRouter({
     history: createWebHashHistory('util-tools'),
     routes: [
         { name: "home", path: "/", component: () => import("../views/HomePage") },
-        { name: "MultipleSearch", path: "/muti-search", component: () => import("../views/MultipleSearch") },
-        { name: "ObsidianVariousComplement", path: "/ob-dict-generator", component: () => import("../views/ObsidianVariousComplement") },
+        { name: "MultipleSearch", path: "/mutiSearch", component: () => import("../views/MultipleSearch") },
+        { name: "ObsidianVariousComplement", path: "/obDictGenerator", component: () => import("../views/ObsidianVariousComplement") },
     ]
 })// 导出 路由 
 export default router
